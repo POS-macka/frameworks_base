e5a5ecd8f8120ee26c694ca9fb60429f5c6fd67c
someone5678 <nemui3353@gmail.com>
2023-06-26 05:57:42 +0900

    base: Guard brightness slider haptic by setting [1/2]
    Change-Id: Ic42c037da8d7cc8378f084ed20205fafc52b95e5
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6305,6 +6305,12 @@ public final class Settings {
         public static final String VOLUME_SLIDER_HAPTIC_FEEDBACK = "volume_slider_haptic_feedback";
 
         /**
+         * Give haptic feedback on brightness slider changes.
+         * @hide
+         */
+        public static final String BRIGHTNESS_SLIDER_HAPTIC_ENABLED = "brightness_slider_haptic_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
